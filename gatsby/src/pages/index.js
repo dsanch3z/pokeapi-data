b9ca2ac763b414e20ba88048e7401d6027be44d3
Daniel Sanchez <jdaniel.saed@gmail.com>
2019-02-17 12:00:16 -0600

    npm run format

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 
-import Layout from '../components/layout'
-import AllPokemon from '../templates/all-pokemon'
+import Layout from "../components/layout"
+import AllPokemon from "../templates/all-pokemon"
 
 const IndexPage = () => (
   <Layout>
